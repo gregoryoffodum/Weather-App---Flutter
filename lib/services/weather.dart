249e0b8d359192b05449690256d918117120e313
Gregory Okechukwu Offodum <gregoryoffodum@gmail.com>
2021-07-30 17:05:38 +0100

    first commit

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,4 +1,33 @@
+import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
+
+const apiKey = '033905cfddf25de70b26286a102a58e2';
+const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
+
 class WeatherModel {
+
+  Future <dynamic> getCityWeather(String cityName) async {
+
+    var url = '$openWeatherMapURL?q=$cityName&appid=$apiKey&units=metric';
+    NetworkHelper networkHelper =NetworkHelper(url);
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+
+  }
+
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+
+    await location.getCurrentLocation();
+
+    NetworkHelper networkHelper = NetworkHelper('$openWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData();
+
+    return weatherData;
+  }
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
@@ -18,6 +47,7 @@ class WeatherModel {
       return '🤷‍';
     }
   }
+
 
   String getMessage(int temp) {
     if (temp > 25) {
